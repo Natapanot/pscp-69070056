0a4577e784ef3ee28b7ba2d5bd30c8e4df09e7b4
Natapanot <69070056@kmitl.ac.th>
2026-07-31 23:21:19 +0700

    PSCP

---
+++ 3011/[LEARNING LOGS] Colors.py
@@ -0,0 +1,14 @@
+"""[LEARNING LOGS] Colors"""
+color1 = input()
+color2 = input()
+
+colors = {a, b}
+
+if colors == {"Red", "Yellow"}:
+    print("Orange")
+elif colors == {"Red", "Blue"}:
+    print("Violet")
+elif colors == {"Yellow", "Blue"}:
+    print("Green")
+else:
+    print("Error")
